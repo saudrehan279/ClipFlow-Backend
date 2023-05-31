5629747847355a26de8dcdf444cc294d618266aa
Saud <saoodrehan@gmail.com>
2023-05-31 12:15:46 +0500

    Commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const youtubedl = require('youtube-dl-exec');
 
 const port = 3000;
 const app = express();
@@ -10,6 +11,23 @@ app.use(cors({
 
 app.get("/", async (req, res) => {
      res.status(200).send("Working!")
+})
+
+app.get("/getUrls", async (req, res) => {
+     try {
+          // let url = "https://www.youtube.com/watch?v=jJvQEtYvwTk";
+          let url = req.query.url
+
+          const options = {
+               dumpSingleJson: true,
+          };
+
+          const data = await youtubedl(url, options);
+
+          res.status(200).send(data)
+     } catch (error) {
+          res.status(500).send(error)
+     }
 })
 
 app.listen(port, () => {
